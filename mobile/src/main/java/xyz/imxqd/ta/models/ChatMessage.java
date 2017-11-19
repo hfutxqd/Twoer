d8c05ec65e1b4641688f79b0e1a3908cbeb21967
imxqd <ahtlxqd@gmail.com>
2017-11-19 23:17:49 +0800

    调整项目结构

---
+++ mobile/src/main/java/xyz/imxqd/ta/models/ChatMessage.java
@@ -0,0 +1,8 @@
+package xyz.imxqd.ta.models;
+
+/**
+ * Created by imxqd on 2017/11/19.
+ */
+
+public class ChatMessage {
+}
---
+++ mobile/src/main/java/xyz/imxqd/ta/ui/adapters/MessageAdapter.java
@@ -0,0 +1,72 @@
+package xyz.imxqd.ta.ui.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import xyz.imxqd.ta.R;
+import xyz.imxqd.ta.models.ChatMessage;
+import xyz.imxqd.ta.ui.fragments.ChatFragment;
+import xyz.imxqd.ta.ui.fragments.ChatFragment.OnChatMessageInteractionListener;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    private final List<ChatMessage> mValues;
+    private final OnChatMessageInteractionListener mListener;
+
+    public MessageAdapter(List<ChatMessage> items, ChatFragment.OnChatMessageInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_chat, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public ChatMessage mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
